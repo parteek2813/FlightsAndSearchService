fc8a1553c95e103ec2f7f7c65b97a996ada20811
parteek kumar <digitalparteek@gmail.com>
2023-06-03 13:20:58 +0530

    initialized sequelize

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 const express = require("express");
-
+const body_parser = require("body-parser");
 // require("dotenv").config();
 // const { PORT } = require("./config/serverConfig");
 
 const setupAndStartServer = async () => {
   //create the express object
   const app = express();
+
+  app.use(body_parser.json());
+  app.use(body_parser.urlencoded({ extended: true })); //Only parses the url-encoded bodies
 
   const PORT = 3000;
   app.listen(PORT, () => {
